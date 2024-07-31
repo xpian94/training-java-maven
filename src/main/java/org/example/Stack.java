aa6ab356aac0ac45d683f67b4f9a8aefade8e230
Cristian <cristian.mello@hotmail.com.br>
2024-07-31 14:57:17 +0000

    feat: adding create empty stack

---
+++ src/main/java/org/example/Stack.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Stack {
+
+    boolean isEmpty() {
+        return true;
+    }
+}
--- src/test/java/org/example/StackTest.java
+++ src/test/java/org/example/StackTest.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class StackTest {
     @Test
     void nothing() {
+        var stack = new Stack();
 
+        assertTrue(stack.isEmpty());
     }
 }
